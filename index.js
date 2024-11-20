98966b3dbcf2b722dc08387d724ec36f994f9828
jasurhaydarovcode <jasurhaydarovcode@gmail.com>
2024-11-20 10:51:51 +0500

    New Data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require( 'express' );
+const express = require('express');
 const app = express();
 const port = 3000;
 const path = require('path')
@@ -8,25 +8,30 @@ app.set('view engine', 'ejs');
 
 // Create a route
 app.get('/', function (req, res) {
-    res.render('index', { title: 'Hello', message: 'This is an EJS example.' });
+    res.render('index', {title: 'Hello', message: 'This is an EJS example.'});
 });
 
 app.use(express.static('public'))
 
 // JSON faylini import qilish
-const shopData = require( './API/YaponOvqatlari.json' );
+const shopData = require('./API/YaponOvqatlari.json');
+const uzbRegions = require('./API/UzbekistonShaxarlari.json')
 
 // JSON uchun middleware
-app.use( express.json() );
+app.use(express.json());
 
 // API endpoint
-app.get( '/api/yaponOvqatlari', ( req, res ) => {
-    res.json( shopData );
-} );
+app.get('/api/yaponOvqatlari', (req, res) => {
+    res.json(shopData);
+});
+
+app.get('/api/uzbekistanShaxarlari', (req, res) => {
+    res.json(uzbRegions);
+});
 
 // Serverni ishga tushirish
-app.listen( port, () => {
-    console.log( `\n API server http://localhost:${ port } da ishga tushdi! \n
- Yapon Ovqatlari http://localhost:${ port }/api/yaponOvqatlari da ishga tushdi!
-    ` );
-} );
+app.listen(port, () => {
+    console.log(`\n API server http://localhost:${port} da ishga tushdi! \n
+ Yapon Ovqatlari http://localhost:${port}/api/yaponOvqatlari da ishga tushdi!
+    `);
+});
